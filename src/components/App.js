176f72e2e65da75cef69ac068be3cec49b19ee8d
harshithadale <daleharshi6045@gmail.com>
2025-07-18 22:02:26 +0530

    initial-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,45 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useEffect, useState } from "react";
+import { initialArray } from '../initialArray';
 
+function App() {
+  const [count, setCount] = useState(1);
+  const [review, setReview] = useState(initialArray[0]);
+
+  useEffect(() => {
+    const obj = initialArray.find((ob) => ob.id === count);
+    setReview(obj);
+  }, [count]);
+  const setRandom =()=>{
+    setCount(Math.floor(Math.random()*initialArray.length)+1)
+  }
+  return (
+    <div>
+      <h1 id="review-heading">Our Reviews</h1>
+      <div className="review">
+        <p className="author" id="author-id">{review.name}</p>
+        <p className="job">{review.job}</p>
+        <p className="info">{review.text}</p>
+        <img src={review.image} width="60" height="60" className="person-img"/>
+      </div>
+      <button
+        onClick={() => {
+          if (count > 1) setCount(count - 1);
+        }}
+        disabled={count === 1}
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => {
+          if (count < initialArray.length) setCount(count + 1);
+        }}
+        disabled={count === initialArray.length}
+      >
+        Next
+      </button>
+      <button className="random-btn" onClick={setRandom}>surprise me</button>
+    </div>
+  );
+}
+
+export default App;
