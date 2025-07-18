b15b2a27436f282115e8fe437ccc23ff23e07332
harshithadale <daleharshi6045@gmail.com>
2025-07-18 22:26:46 +0530

    fifth-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { initialArray } from '../initialArray';
+import { initialArray } from "../initialArray";
 
 function App() {
   const [count, setCount] = useState(1);
@@ -9,33 +9,51 @@ function App() {
     const obj = initialArray.find((ob) => ob.id === count);
     setReview(obj);
   }, [count]);
-  const setRandom =()=>{
-    setCount(Math.floor(Math.random()*initialArray.length)+1)
-  }
+
+  const setRandom = () => {
+    let randomId;
+    do {
+      randomId = Math.floor(Math.random() * initialArray.length) + 1;
+    } while (randomId === count);
+    setCount(randomId);
+  };
+
   return (
     <div>
       <h1 id="review-heading">Our Reviews</h1>
-      <div className="review">
-        <p className="author" id={`author-${review.id}`}>{review.name}</p>
-        <p className="job">{review.job}</p>
-        <p className="info">{review.text}</p>
-        <img src={review.image} width="60" height="60" className="person-img"/>
-         <button className="prev-btn"
-        onClick={() => {
-          if (count > 1) setCount(count - 1);
-        }}
-      >
-        Prev
-      </button>
-      <button  className="next-btn"
-        onClick={() => {
-          if (count < initialArray.length) setCount(count + 1);
-        }}
-      >
-        Next
-      </button>
-       <button className="random-btn" onClick={setRandom}>surprise me</button>
-      </div>
+      {review && (
+        <div className="review">
+          <p className="author" id={`author-${review.id}`}>{review.name}</p>
+          <p className="job">{review.job}</p>
+          <p className="info">{review.text}</p>
+          <img
+            src={review.image}
+            width="60"
+            height="60"
+            className="person-img"
+            alt={review.name}
+          />
+          <button
+            className="prev-btn"
+            onClick={() => {
+              if (count > 1) setCount(count - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button
+            className="next-btn"
+            onClick={() => {
+              if (count < initialArray.length) setCount(count + 1);
+            }}
+          >
+            Next
+          </button>
+          <button className="random-btn" onClick={setRandom}>
+            Surprise Me
+          </button>
+        </div>
+      )}
     </div>
   );
 }
